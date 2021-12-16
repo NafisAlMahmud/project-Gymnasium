5704b1e8dd6794bb91da86bad696e342388403bb
Project_3 <anik2115@gmail.com>
2021-12-17 04:03:39 +0600

    addfile

--- js/custom.js
+++ js/custom.js
@@ -10,11 +10,22 @@ $(document).ready(function(){
     // about video pop up
     $('.venobox').venobox();
 
+    
+
     // banner slider;
     $('.testimonial-slider').slick({
         arrows: false,
         dots: true,
         slidesToShow :2,
+    });
+    $('.brand-slider').slick({
+        arrows: true,
+        
+        slidesToShow :5,
+        centerMode: true,
+        centerPadding: '0px',
+        prevArrow: '<i class = "fas fa-chevron-left prevArrow"></i>',
+        nextArrow: '',
     });
 
 
